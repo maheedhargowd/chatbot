25f60f7fd68262ae8b4e868c20f1887bc9d13781
unknown <maheedhar gowd>
2024-08-21 01:34:38 +0530

    Syncing local changes with GitHub

--- llmchat/settings.py
+++ llmchat/settings.py
@@ -136,7 +136,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Print the STATIC_ROOT path for debugging
+print("STATIC_ROOT is set to:", STATIC_ROOT)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
